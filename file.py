ec24e0ee796111621e7fc114ce94a2a1dd43d382
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-11 16:59:40 -0600

    Update file.py

--- file.py
+++ file.py
@@ -1,6 +1,8 @@
 import os
 
-if not os.path.exists('extras'):
+main_path = os.envrion.get('HOME')
+
+if not os.path.exists(main_path + '/extras'):
   os.system('cd && git clone https://github.com/ARACADERISE/extras')
 
 os.system('cd extras/python/yal_lang && python file.py')
