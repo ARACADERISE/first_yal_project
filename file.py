d7590ebce15424b3b62713915d23129f84010cc8
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-15 09:23:11 -0600

    Update file.py

--- file.py
+++ file.py
@@ -7,4 +7,5 @@ if not os.path.exists(main + '/extras'):
 	os.system('cd extras/python/yal_lang && python file.py')
 else:
 	os.system(f'cd {main} && rm -rf extras')
-	os.system('git clone https://github.com/ARACADERISE/extras && cd extras/python/yal_lang && python file.py')
+	if not os.path.exists('extras'):
+		os.system('git clone https://github.com/ARACADERISE/extras && cd extras/python/yal_lang && python file.py')
